693608774057daf5a8e96857a70aaf20d7cbe8e5
Daniel Rodríguez Troitiño <drodrigueztroitino@yahoo.es>
2009-01-10 16:14:47 +0000

    New COPEAppDelegate (copied from radio3AppDelegate and FIAppDelegate).

--- Classes/COPEAppDelegate.m
+++ Classes/COPEAppDelegate.m
@@ -7,8 +7,63 @@
 //
 
 #import "COPEAppDelegate.h"
+#import <AudioToolbox/AudioToolbox.h>
+#import "COPEViewController.h"
+#import "Reachability.h"
 
+static NSString *kCOPEHost = @"cope.stream.flumotion.com";
+
+void interruptionListenerCb(void *inClientData, UInt32 interruptionState) {
+  COPEViewController *controller = (COPEViewController *) inClientData;
+  [controller audioSessionInterruption:interruptionState];
+}
 
 @implementation COPEAppDelegate
+
+@synthesize window;
+@synthesize viewController;
+
+- (void)applicationDidFinishLaunching:(UIApplication *)application {
+  [[Reachability sharedReachability] setHostName:kCOPEHost];
+  [[Reachability sharedReachability] setNetworkStatusNotificationsEnabled:YES];
+  
+  OSStatus err = AudioSessionInitialize(NULL, NULL,
+                                        interruptionListenerCb,
+                                        viewController);
+  if (err != kAudioSessionNoError) {
+    RNLog(@"AudioSessionInitialize error %d", err);
+  }
+  
+  UInt32 sessionCategory = kAudioSessionCategory_MediaPlayback;
+  err = AudioSessionSetProperty(kAudioSessionProperty_AudioCategory,
+                                sizeof(sessionCategory),
+                                &sessionCategory);
+  if (err != kAudioSessionNoError) {
+    RNLog(@"AudioSessionSetProperty error %d", err);
+  }
+  
+  err = AudioSessionSetActive(YES);
+  if (err != kAudioSessionNoError) {
+    RNLog(@"AudioSessionSetActive error %d", err);
+  }
+  
+  [window addSubview:viewController.view];
+  [window makeKeyAndVisible];
+  
+  [[NSNotificationCenter defaultCenter] addObserver:viewController
+                                           selector:@selector(reachabilityChanged:)
+                                               name:@"kNetworkReachabilityChangedNotification"
+                                             object:nil];
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+  [viewController saveApplicationState];
+}
+
+- (void)dealloc {
+  [viewController release];
+  [window release];
+  [super dealloc];
+}
 
 @end
