04abd024dea8324e7a4cc6a16e0ce75eff7f47b7
Javier Quevedo Fernández <jquevedo@gmail.com>
2009-01-10 16:06:28 +0000

    Included cope from FIRADOI in COPEViewController

--- Classes/COPEViewController.h
+++ Classes/COPEViewController.h
@@ -6,11 +6,28 @@
 //  Copyright 2009 Daniel Rodríguez and Javier Quevedo. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
 
+#import <UIKit/UIKit.h>
+#import <MediaPlayer/MediaPlayer.h>
+
+@class Player;
 
 @interface COPEViewController : UIViewController {
-
+@private
+	IBOutlet UIButton *controlButton;
+	IBOutlet UIView *volumeViewHolder;
+	IBOutlet UIImageView *loadingImage;
+	
+	Player *player;
+	
+	BOOL isPlaying;
+	BOOL interruptedDuringPlayback;
+	
+	UIView *volumeSlider;
 }
+
+- (IBAction)controlButtonClicked:(UIButton *)button;
+
+- (void)audioSessionInterruption:(UInt32)interruptionState;
 
 @end
--- Classes/COPEViewController.m
+++ Classes/COPEViewController.m
@@ -7,40 +7,177 @@
 //
 
 #import "COPEViewController.h"
+#import "AudioClass.h"
+#import "Reachability.h"
 
 
-@implementation COPEViewController
+NSString *kFIFMRadioURL = @"http://cope.stream.flumotion.com/cope/copefm.mp3.m3u";
 
-/*
-// The designated initializer. Override to perform setup that is required before the view is loaded.
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
-    if (self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]) {
-        // Custom initialization
-    }
-    return self;
+// NSString *kFIFMRadioURL = @"http://scfire-ntc-aa10.stream.aol.com:80/stream/1040";
+
+@interface FIViewController ()
+
+- (void)stopRadio;
+
+- (void)playRadio;
+- (void)privatePlayRadio;
+
+- (void)showNetworkProblemsAlert;
+
+- (void)reachabilityChanged:(NSNotification *)notification;
+
+@end
+
+@implementation FIViewController
+
+#pragma mark IBActions
+
+- (IBAction)controlButtonClicked:(UIButton *)button {
+	if (isPlaying) {
+		[self stopRadio];
+	} else {
+		[self playRadio];
+	}
 }
-*/
 
-/*
-// Implement loadView to create a view hierarchy programmatically, without using a nib.
-- (void)loadView {
+#pragma mark Custom methods
+
+- (void)audioSessionInterruption:(UInt32)interruptionState {
+	if (interruptionState == kAudioSessionBeginInterruption) {
+		BOOL playing = isPlaying;
+		[self stopRadio];
+		AudioSessionSetActive(NO);
+		interruptedDuringPlayback = playing;
+	} else if (interruptionState == kAudioSessionEndInterruption) {
+		AudioSessionSetActive(YES);
+		if (interruptedDuringPlayback)
+			[self playRadio];
+		interruptedDuringPlayback = NO;
+	}
 }
-*/
 
-/*
-// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
+- (void)reachabilityChanged:(NSNotification *)notification {
+	if ([[Reachability sharedReachability] remoteHostStatus] == NotReachable) {
+		[self showNetworkProblemsAlert];
+	}
+}
+
+- (void)showNetworkProblemsAlert {
+	UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Problemas de conexión"
+														message:@"No es posible conectar a Internet.\nAsegurese de disponer de conexión a Internet."
+													   delegate:nil
+											  cancelButtonTitle:nil
+											  otherButtonTitles:@"Aceptar", nil];
+	[alertView show];
+}
+
+- (void)stopRadio {
+	if (isPlaying) {
+		[player stop];
+	}
+}
+
+- (void)setPlayState {
+	
+}
+
+- (void)setStopState {
+	
+}
+
+- (void)setFailedState:(NSError *)error {
+	// If we loose network reachability both callbacks will get call, so we
+	// step aside if a network lose has happened.
+	if ([[Reachability sharedReachability] remoteHostStatus] == NotReachable) {
+		// The reachability callback will show its own AlertView.
+		return;
+	}
+	
+	NSString *message;
+	if (error != nil) {
+		message = [NSString stringWithFormat:@"Ha sucedido un error \"%@\".\nLo sentimos mucho.", error.localizedDescription];
+	} else {
+		message = @"Ha sucedido un error.\nLo sentimos mucho.";
+	}
+	
+	UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Problemas"
+														message:message
+													   delegate:nil
+											  cancelButtonTitle:nil
+											  otherButtonTitles:@"Aceptar", nil];
+	[alertView show];
+}
+
+- (void)setLoadingState {
+	
+}
+
+- (void)playRadio {
+	if ([[Reachability sharedReachability] remoteHostStatus] == NotReachable) {
+		[self showNetworkProblemsAlert];
+		return;
+	}
+	
+	if (isPlaying) {
+		return;
+	}
+	
+	[NSThread detachNewThreadSelector:@selector(privatePlayRadio)
+							 toTarget:self
+						   withObject:nil];
+}
+
+- (void)privatePlayRadio {
+	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+	
+	[self performSelector:@selector(setLoadingState)
+				 onThread:[NSThread mainThread]
+			   withObject:nil
+			waitUntilDone:NO];
+	
+	player = [[Player alloc] initWithString:kFIFMRadioURL audioTypeHint:kAudioFileMP3Type];
+	player.connectionFilter = [[FIShoutcastMetadataFilter alloc] init];
+	
+	[player addObserver:self forKeyPath:@"isPlaying" options:0 context:nil];
+	[player addObserver:self forKeyPath:@"failed" options:0 context:nil];
+	[player start];
+	
+	isPlaying = YES;
+	
+	[pool release];
+}
+
+
+- (void)volumeChanged:(NSNotification *)notify {
+	RNLog(@"volume changed");
+	[volumeSlider _updateVolumeFromAVSystemController];
+}
+
+#pragma mark UIViewController methods
+
 - (void)viewDidLoad {
-    [super viewDidLoad];
+	MPVolumeView *volumeView =
+    [[[MPVolumeView alloc] initWithFrame:volumeViewHolder.bounds] autorelease];
+	[volumeView sizeToFit];
+	[volumeViewHolder addSubview:volumeView];
+	
+	// Find the slider
+	volumeSlider = [volumeView valueForKey:@"_volumeSlider"];
+	
+	[[NSNotificationCenter defaultCenter] addObserver:self
+											 selector:@selector(volumeChanged:)
+												 name:@"AVSystemController_SystemVolumeDidChangeNotification"
+											   object:nil];
+	
+	[super viewDidLoad];
 }
-*/
 
-/*
-// Override to allow orientations other than the default portrait orientation.
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
-    // Return YES for supported orientations
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+- (void)viewDidAppear:(BOOL)animated {
+	// Needed to start receiving reachability status notifications
+	[[Reachability sharedReachability] remoteHostStatus];
+	
+	[super viewDidAppear:animated];
 }
-*/
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning]; // Releases the view if it doesn't have a superview
@@ -49,7 +186,7 @@
 
 
 - (void)dealloc {
-    [super dealloc];
+	[super dealloc];
 }
 
 
