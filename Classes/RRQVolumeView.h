69a3ae204ce2adfde47e3757f3a82b0a3c8a00fb
Daniel Rodríguez Troitiño <drodrigueztroitino@yahoo.es>
2009-04-07 23:18:25 +0200

    Subclass of MPVolumeView to avoid SDK 3 problems.

---
+++ Classes/RRQVolumeView.h
@@ -0,0 +1,16 @@
+//
+//  RRQVolumeView.h
+//  radio3
+//
+//  Created by Daniel Rodríguez Troitiño on 07/04/09.
+//  Copyright 2009 Javier Quevedo and Daniel Rodríguez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <MediaPlayer/MediaPlayer.h>
+
+@interface RRQVolumeView : MPVolumeView
+
+- (void)finalSetup;
+
+@end
