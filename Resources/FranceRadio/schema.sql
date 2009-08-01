e989c3176db082c8ff6e5818ca9836a92c18bb51
Daniel Rodríguez Troitiño <drodrigueztroitino@yahoo.es>
2009-08-01 12:00:47 +0000

    New database schema.

--- Resources/FranceRadio/schema.sql
+++ Resources/FranceRadio/schema.sql
@@ -3,17 +3,15 @@ CREATE TABLE "SQLITESEQUENCE" ("name" TEXT, "seq" INTEGER);
 CREATE INDEX "SQLITESEQUENCE_name" ON "SQLITESEQUENCE" ("name");
 
 /* Table view items */
-CREATE TABLE "table_view_items" (
+CREATE TABLE "directory_items" (
   "pk" INTEGER PRIMARY KEY,
   "parent" INTEGER,
   "position" INTEGER,
-  "group" TEXT,
-  "radio_group" TEXT,
   "radio" TEXT
 );
-CREATE INDEX "table_view_items_parent" ON "table_view_items" ("parent");
-CREATE INDEX "table_view_items_parent_position" ON "table_view_items" ("parent", "position");
-INSERT INTO "SQLITESEQUENCE" ("name", "seq") VALUES ('table_view_items', 0);
+CREATE INDEX "directory_items_parent" ON "directory_items" ("parent");
+CREATE INDEX "directory_items_parent_position" ON "directory_items" ("parent", "position");
+INSERT INTO "SQLITESEQUENCE" ("name", "seq") VALUES ('directory_items', 0);
 
 /* Radios */
 CREATE TABLE "radios" (
@@ -27,10 +25,19 @@ INSERT INTO "SQLITESEQUENCE" ("name", "seq") VALUES ('radios', 0);
 /* Radio groups */
 CREATE TABLE "radio_groups" (
   "pk" INTEGER PRIMARY KEY,
-  "group_name" TEXT,
-  "selected" TEXT
+  "name" TEXT
 );
 INSERT INTO "SQLITESEQUENCE" ("name", "seq") VALUES ('radio_groups', 0);
+
+/* Favorites */
+CREATE TABLE "favorites" (
+  "pk" INTEGER PRIMARY KEY,
+  "position" INTEGER,
+  "last_used_at" TEXT,
+  "radio" TEXT
+);
+CREATE INDEX "favorites_position" ON "favorites" ("position");
+INSERT INTO "SQLITESEQUENCE" ("name", "seq") VALUES ('favorites', 0);
 
 /* Version table */
 CREATE TABLE "db_schema" (
